febc54fc6fbadf86d5480df784bbbace3d9f1280
z-m-kanylbay <z.m.kanylbay@viarune.com>
2024-02-28 18:58:56 +0700

    set widgets and validtor class

--- Square/win.cpp
+++ Square/win.cpp
@@ -3,6 +3,38 @@
 Win::Win(QWidget *parent)
     : QWidget(parent)
 {
+    setWindowTitle("Возведение в квадрат");
+    frame = new QFrame(this);
+    frame->setFrameShadow(QFrame::Raised);
+    frame->setFrameShape(QFrame::Panel);
+
+    inputLabel  = new QLabel("Введите число:", this);
+    inputEdit   = new QLineEdit("",this);
+
+    StrValidator *v=new StrValidator(inputEdit);
+    inputEdit->setValidator(v);
+
+    outputLabel = new QLabel("Результат:", this);
+    outputEdit  = new QLineEdit("",this);
+    nextButton  = new QPushButton("Следующее", this);
+    exitButton  = new QPushButton("Выход", this);
+
+    // компоновка приложения выполняется согласно рисунку 2.
+    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
+    vLayout1->addWidget(inputLabel);
+    vLayout1->addWidget(inputEdit);
+    vLayout1->addWidget(outputLabel);
+    vLayout1->addWidget(outputEdit);
+    vLayout1->addStretch();
+
+    QVBoxLayout *vLayout2 = new QVBoxLayout();
+    vLayout2->addWidget(nextButton);
+    vLayout2->addWidget(exitButton);
+    vLayout2->addStretch();
+
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    hLayout->addWidget(frame);
+    hLayout->addLayout(vLayout2);
 }
 
 Win::~Win()
--- Square/win.h
+++ Square/win.h
@@ -2,13 +2,31 @@
 #define WIN_H
 
 #include <QWidget>
+#include <QtWidgets>
 
 class Win : public QWidget
 {
     Q_OBJECT
-
+protected:
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
     Win(QWidget *parent = nullptr);
     ~Win();
+};
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
 };
 #endif // WIN_H
