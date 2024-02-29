5632909d2d8be483d675fadb8a3ba9da1ecbe2c5
z-m-kanylbay <z.m.kanylbay@viarune.com>
2024-02-29 12:00:44 +0700

    ex 2.8

--- Counter/win.h
+++ Counter/win.h
@@ -2,6 +2,30 @@
 #define WIN_H
 
 #include <QWidget>
+#include <QtWidgets>
+
+class Counter:public QLineEdit
+{
+    Q_OBJECT
+public:
+    Counter(const QString & contents, QWidget *parent = 0):
+        QLineEdit(contents, parent)
+    {
+        this->setReadOnly(true);
+    }
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str = text();
+        int r = str.toInt();
+        r++;
+        if (r != 0 && r % 5 == 0) emit tick_signal();
+        str.setNum(r);
+        setText(str);
+    }
+};
 
 class Win : public QWidget
 {
@@ -10,5 +34,10 @@ class Win : public QWidget
 public:
     Win(QWidget *parent = nullptr);
     ~Win();
+protected:
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
 };
 #endif // WIN_H
