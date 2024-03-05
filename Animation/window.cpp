ee604180785fc3235269fb26f9f7e58c8360d94c
insert-zkm <zkanylbaev03@gmail.com>
2024-03-05 13:35:25 +0700

    eliminated unecessary deletion of area

--- Animation/window.cpp
+++ Animation/window.cpp
@@ -8,15 +8,15 @@ Window::Window(QWidget *parent)
     this->setWindowTitle("Обработка событий");
     area = new Area(this);
     btn = new QPushButton("Завершить", this);
+
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(area);
     layout->addWidget(btn);
 
-    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+    connect(btn, &QPushButton::clicked, this, &Window::close);
 }
 
 Window::~Window()
 {
-    delete area;
 }
 
