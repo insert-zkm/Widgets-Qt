07d93ec094297e571e5ad8de01ec094e54613b53
insert-zkm <zkanylbaev03@gmail.com>
2024-03-16 10:46:07 +0700

    wrap main function with try-catch

--- Counter/main.cpp
+++ Counter/main.cpp
@@ -2,10 +2,12 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) try
 {
     QApplication a(argc, argv);
     Win w;
     w.show();
     return a.exec();
+} catch (const std::exception &e) {
+    qDebug() << e.what();
 }
